2148e15fea3829c1d00c8cd351de824989328815
Vladykoo <Vladykoo@mail.ru>
2020-10-30 15:14:41 +0300

    Remove request,service and change package,json

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {RequestService} from '../request.service';
+import {RequestService} from '../table/request.service';
 
 @Component({
   selector: 'app-table',
